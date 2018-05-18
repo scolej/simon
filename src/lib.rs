df24ec7ca1036b8e974979b18d1461067d6b04ce
Dylan Maccora <maccora17@gmail.com>
2018-05-18 16:24:33 +1000

    Adding auth from dotenv

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate dotenv;
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
+#[macro_use]
+extern crate hyper;
 extern crate rand;
 extern crate reqwest;
 extern crate serde;
--- src/provider/travis.rs
+++ src/provider/travis.rs
@@ -1,22 +1,41 @@
 use super::ProviderApi;
 use model::{Build, BuildId, BuildQuery, BuildResponse, BuildStatus};
-use reqwest;
+use reqwest::{self, header};
 use failure::Error;
 use std::convert::From;
 use std::time::Duration;
+use dotenv::dotenv;
+use std::env;
 
 const URL: &str = "https://api.travis-ci.org";
 pub struct TravisApi;
 
+header!{(TravisVersion, "Travis-API-Version") => [String]}
+impl TravisApi {
+    fn headers(&self) -> header::Headers {
+        let mut headers = header::Headers::new();
+        dotenv().ok();
+        let travis_token = env::var("TRAVIS_TOKEN").expect("Could not locate token for Travis");
+        headers.set(header::Authorization("token ".to_owned() + &travis_token));
+        headers.set(header::UserAgent::new("API Explorer"));
+        headers.set(TravisVersion("3".to_owned()));
+        headers
+    }
+}
+
 impl ProviderApi for TravisApi {
     fn build_status(&self, query: BuildQuery) -> Result<BuildResponse, Error> {
-        // TODO: Add the authorization from dotenv
+        let client = reqwest::Client::new();
         let api = format!(
             "{}/repo/{}%2F{}/branch/{}",
             URL, query.namespace, query.project, query.branch
         );
+
+        let headers = self.headers();
         println!("Hitting {}", api);
-        let result: TravisResponse = reqwest::get(&api)?.json()?;
+        let mut res = client.get(&api).headers(headers).send()?;
+        println!("{:?}", res);
+        let result: TravisResponse = res.json()?;
         println!("{:?}", result);
         Ok(result.into())
     }
